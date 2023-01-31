fcbcd09729a17225e4003df923935f9b3e9cf1a0
Robert <rguerrra.training@wowdesarrollos.com>
2023-01-31 17:07:45 -0500

    forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,111 @@
 import "./App.css";
 import { registerUser } from "./services/registerUser";
-
+import { useForm } from "react-hook-form";
 export function App() {
+  const {
+    register,
+    formState: { errors, isDirty, isValid },
+    handleSubmit,
+    watch,
+    getValues,
+  } = useForm({ mode: "onChange" });
   const onSubmit = (data) => {
     registerUser(data);
   };
-
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>
             Email
-            <input type="email" placeholder="Email" />
+            <input
+              type="email"
+              placeholder="Email"
+              {...register("email", {
+                required: true,
+                pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+              })}
+            />
           </label>
-          <span className="error" role="alert">
-            Test error
-          </span>
+          {errors?.email?.type === "required" && (
+            <span className="error" role="alert">
+              email is required
+            </span>
+          )}
+          {errors?.email?.type === "pattern" && (
+            <span className="error" role="alert">
+              email is invalid
+            </span>
+          )}
         </div>
         <div>
           <label>
             Name
-            <input type="text" placeholder="Name" />
+            <input
+              {...register("name", {
+                required: true,
+              })}
+              placeholder="Name"
+            />
+            {errors?.name?.type === "required" && <p>name is required</p>}
           </label>
-          <span className="error" role="alert"></span>
         </div>
         <div>
           <label>
             Age
-            <input type="number" placeholder="Age" />
+            <input
+              type="number"
+              placeholder="Age"
+              {...register("age", {
+                required: true,
+                min: 18,
+              })}
+            />
           </label>
-          <span className="error" role="alert"></span>
+          {errors?.age?.type === "required" && <p>age is required</p>}
+          {errors?.age?.type === "min" && (
+            <p>you must be above 18 to register</p>
+          )}
         </div>
         <div>
           <label>
             Password
-            <input type="password" placeholder="Password" />
+            <input
+              type="password"
+              placeholder="Password"
+              {...register("password", {
+                required: true,
+                minLength: 5,
+              })}
+            />
           </label>
-          <span className="error" role="alert"></span>
+          {errors?.password?.type === "required" && <p>password is required</p>}
+          {errors?.password?.type === "minLength" && (
+            <p>password is too short</p>
+          )}
         </div>
         <div>
           <label>
             Password check
-            <input type="password" placeholder="Password check" />
+            <input
+              type="password"
+              placeholder="Password check"
+              {...register("passwordCheck", {})}
+            />
           </label>
-          <span className="error" role="alert"></span>
+          {watch("passwordCheck") !== watch("password") &&
+          getValues("passwordCheck") ? (
+            <p>passwords do not match</p>
+          ) : null}
         </div>
         <div>
           <label>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              {...register("checkbox", {
+                required: true,
+              })}
+            />
             Accept terms & conditions: Lorem ipsum dolor sit amet, consectetur
             adipiscing elit. Pellentesque pharetra, tortor ac placerat
             elementum, neque libero luctus mi, ut efficitur nisl mauris at nisl.
@@ -56,10 +113,13 @@ export function App() {
             magna, sollicitudin eu porttitor ut, tincidunt sit amet urna.
             Vestibulum congue neque metus.
           </label>
-          <span className="error" role="alert"></span>
+          {errors?.checkbox?.type === "required" && (
+            <p>please read and accept the terms and conditions</p>
+          )}
         </div>
-
-        <button>Sign up</button>
+        <button type="submit" disabled={!isDirty || !isValid}>
+          Sign up
+        </button>
       </form>
     </div>
   );
